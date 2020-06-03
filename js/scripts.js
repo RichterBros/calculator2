cfb1b171fa015858783dbe861be585d6181005d8
Erich Richter <erich.richter@gmail.com>
2020-06-03 16:06:11 -0700

    initial commit > > co-authored-by: Taylor Somers <tsomers@yandex.com> co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,53 @@
 const add = function(number1, number2) {
-  return number1 + number2;
-};
-
-const subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-const multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-const divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-$(document).ready(function(){
-
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-
+    return number1 + number2;
+  };
+  
+  const subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  const multiply = function(number1, number2) {
+    return number1 * number2;
+  // Business logic:
+  
+  const add = function(number1, number2) {
+      return number1 + number2;
+    };
+    
+    const subtract = function(number1, number2) {
+      return number1 - number2;
+    };
+    
+    const multiply = function(number1, number2) {
+      return number1 * number2;
+    };
+    
+    const divide = function(number1, number2) {
+      return number1 / number2;
+    };
+    
+    // User interface logic:
+    
+    $(document).ready(function(){
+    
+      $("form#add").submit(function(event) {
+        event.preventDefault();
+        const number1 = parseInt($("#add1").val());
+        const number2 = parseInt($("#add2").val());
+        const result = add(number1, number2);
+        $("#output").text(result);
+      });
+    
+      $("form#subtract").submit(function(event) {
+        event.preventDefault();
+        const number1 = parseInt($("#subtract1").val());
+        const number2 = parseInt($("#subtract2").val());
+        const result = subtract(number1, number2);
+        $("#output").text(result);
+      });
+    
+    
+        
+        
+        
+    });
